df6563aa9e29a26f632e188f1d95d88c31271641
MasterQuanh <quanganhdang2004@gmail.com>
2024-12-12 14:05:51 +0700

    Add_featured_product_card

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -8,9 +8,14 @@ module.exports = {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
         },
+        fadeIn: {
+          from : {opacity :0},
+          to : { opacity :1}
+        }
       },
       animation: {
         wiggle: 'slideDown .4s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out'
       },
       colors: {
         'coffee': {
